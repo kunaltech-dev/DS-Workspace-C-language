07afc05e7252c678791634b64b65eede1c176fff
kunal kumar <kunalkr620593@gmail.com>
2026-06-10 07:41:46 +0530

    Singly Linked List insertion From End position

---
+++ 02_Singly_Linked_List_insertion_from_end_position.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *temp, *head = NULL;
+
+void insert_E(int value)
+{
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = value;
+    new_node->next = NULL;
+    if (head == NULL){
+        head = new_node;
+        return;
+    }
+    else{
+        temp = head;
+        while (temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+
+void display(){
+    temp = head;
+    if (head == NULL){
+        printf("\nLinked list is empty !");
+        return;
+    }
+    while (temp != NULL){
+        printf("%d-> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void reverse_d(){
+    if (head == NULL){
+        printf("\nLinked list is empty !");
+        return;
+    }
+    int count = 0;
+    temp = head;
+    while (temp != NULL){
+        count++;
+        temp = temp->next;
+    }
+    printf("NULL");
+    for (int i = count; i >= 1; i--){
+        temp = head;
+        for (int j = 1; j < i; j++){
+            temp = temp->next;
+        }
+        printf(" <-%d", temp->data);
+    }
+    printf("\n");
+}
+
+int main(){
+    int v, num;
+    char extra;
+    printf("Enter: 0 -> terminate program\n\n1 -> insertion from End\n2 -> display \n3 -> reverse display\n");
+    printf("\n");
+    while (1){
+        while (1){
+            printf("\nEnter Your choice= ");
+            if (scanf("%d%c", &num, &extra) == 2 && extra == '\n'){
+                if (num >= 0 && num <= 3){
+                    break;
+                }
+                else{
+                    printf("Invalid range! Enter any number from 0 to 3.\n");
+                }
+            }
+            else{
+                printf("Invalid input! Enter only an integer.\n");
+                while (getchar() != '\n')
+                    ;
+            }
+        }
+        switch (num){
+
+        case 0: printf("Program terminate Successfully ");
+                return 0;
+
+        case 1: printf("Enter value=");
+                scanf("%d", &v);
+                insert_E(v);
+                break;
+
+        case 2: display();
+                break;
+
+        case 3: reverse_d();
+                break;
+        }
+    }
+    return 0;
+}
