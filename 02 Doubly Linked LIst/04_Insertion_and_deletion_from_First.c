0591c3bc6e6cedac70a44fdbd4de62e81c382f45
kunal kumar <kunalkr620593@gmail.com>
2026-06-17 08:17:29 +0530

    Doubly Linked List Insertion at First Position and Deletion from First Position

---
+++ 02 Doubly Linked LIst/04_Insertion_and_deletion_from_First.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+struct node *head = NULL;
+
+void insertion_F(int value){
+    struct node *new;
+    new = (struct node *)malloc(sizeof(struct node));
+    new->data = value;
+    new->prev = NULL;
+    new->next = head;
+    if (head != NULL){
+        head->prev = new;
+    }
+    head = new;
+}
+
+void deletion_F(){
+    struct node *temp = head;
+    if (head == NULL){
+        printf("Linked List is Empty! Please Insertion first\n");
+        return;
+    }
+    else{
+        if (temp->next == NULL){
+            head = temp->next;
+            printf("%d has been deleted!\n", temp->data);
+            free(temp);
+            return;
+        }
+        else{
+            temp->next->prev = NULL;
+            head = temp->next;
+            printf("%d has been deleted!\n", temp->data);
+            free(temp);
+        }
+    }
+}
+
+void display(){
+    struct node *temp = head;
+    if (head == NULL){
+        printf("Linked List is Empty!\n");
+        return;
+    }
+    printf("Doubly Linked List is :\n");
+    printf("NULL<->");
+    while (temp != NULL){
+        printf("%d<->", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void reverse_d(){
+    struct node *temp;
+    if (head == NULL){
+        printf("Linked list is empty !\n");
+        return;
+    }
+    temp = head;
+    printf("Reverse Doubly Linked List:\n");
+    printf("NULL<->");
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+    while (temp != NULL){
+        printf("%d<->", temp->data);
+        temp = temp->prev;
+    }
+    printf("NULL");
+
+    printf("\n");
+}
+
+void freeList(){
+    struct node *temp;
+    while (head != NULL){
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+    printf("Memory freed successfully.\n");
+}
+
+int main(){
+    int v, num;
+    char extra;
+    printf("Enter: \n0 -> terminate program\n1 -> insertion from beggning\n2 -> Deletion from first\n3 -> display \n4 -> reverse display\n");
+    while (1){
+        while (1){
+            printf("\nEnter Your choice= ");
+            if (scanf("%d%c", &num, &extra) == 2 && extra == '\n'){
+                if (num >= 0 && num <= 4){
+                    break;
+                }
+                else{
+                    printf("Invalid range! Enter any number from 0 to 4.\n");
+                }
+            }
+            else{
+                printf("Invalid input! Enter only an integer.\n");
+                while (getchar() != '\n');
+            }
+        }
+        switch (num){
+
+        case 0:
+            freeList();
+            printf("Program terminate Successfully ");
+            return 0;
+
+        case 1:
+            printf("Enter value=");
+            scanf("%d", &v);
+            insertion_F(v);
+            break;
+
+        case 2:
+            deletion_F();
+            break;
+
+        case 3:
+            display();
+            break;
+
+        case 4:
+            reverse_d();
+            break;
+        }
+    }
+    return 0;
+}
